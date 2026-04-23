e5ac64f46e1ec219672955d50f909283fae89a1e
hari6300 <24881A0506@student.vardhaman.org>
2026-04-23 13:48:30 +0530

    Update fetch.php

--- fetch.php
+++ fetch.php
@@ -1,11 +1,18 @@
 <?php
-$conn = new mysqli("localhost","root","","WaterMonitoring");
-$result = $conn->query("SELECT id,data FROM WaterUsage");
+$conn = new mysqli("localhost", "root", "", "WaterMonitoring");
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$result = $conn->query("SELECT * FROM WaterUsage");
 
 $data = [];
-while($row = $result->fetch_assoc()){
- $json = json_decode($row['data'],true);
- $data[] = $json;
+
+while ($row = $result->fetch_assoc()) {
+    $json = json_decode($row['data'], true);
+    $data[] = $json;
 }
+
 echo json_encode($data);
 ?>
